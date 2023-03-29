d9cad41239a82ed78c4d372b225918d33f218d28
jkosciolek <jkosciolek@griddynamics.com>
2023-03-29 11:45:41 +0200

    Task read_write done

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -13,3 +13,22 @@ Example:
 
     result.txt(content: "23, 78, 3")
 """
+import os
+
+
+if __name__ == '__main__':
+    text = []
+
+    # I need to delete .DS file, because it is automatically created on macOs
+    os.remove('files/.DS_Store')
+
+    # read
+    entries = os.listdir('files/')
+    for entry in entries:
+        with open(f'files/{entry}', 'r') as f:
+            text.append(f.read())
+
+    # write
+    with open('files/result.txt', 'x', encoding="utf-8") as f:
+        text_to_file = ', '.join(text)
+        f.write(text_to_file)
